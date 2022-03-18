3837ba7a5b1db8992309a0759551499c5f44979e
Paulo Sousa <paulopgss@gmail.com>
2022-03-18 12:26:39 -0300

    feat: :sparkles: error treatment

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import "express-async-errors";
 import { routes } from "./routes";
 import { logger } from "./util";
 
@@ -7,5 +8,22 @@ const app = express();
 app.use(express.json());
 
 app.use(routes);
+
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof Error) {
+      logger.error(err);
+      response.status(500).json({
+        message: err.message,
+        stack: err.stack,
+      });
+    }
+
+    return response.status(500).json({
+      message: "Internal server error",
+      status: "error",
+    });
+  }
+);
 
 app.listen(3000, () => logger.info("Listening on port 3000"));
