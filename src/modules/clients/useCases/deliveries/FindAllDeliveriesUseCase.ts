1b186888069ce58c3ed7039d972ab10a000ba680
Paulo Sousa <paulopgss@gmail.com>
2022-03-21 20:31:30 -0300

    feat: :sparkles: Searching for deliveries using the customer's token

---
+++ src/modules/clients/useCases/deliveries/FindAllDeliveriesUseCase.ts
@@ -0,0 +1,12 @@
+import { prisma } from "../../../../database/prismaClient";
+
+export class FindAllDeliveriesUseCase {
+  async execute(id_client: string) {
+    const deliveries = await prisma.clients.findMany({
+      where: { id: id_client },
+      include: { deliveries: true },
+    });
+
+    return deliveries;
+  }
+}
--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,7 @@ import { ensureAuthenticateDeliveryman } from "./middlewares/ensureAuthenticateD
 import { AuthenticateClientController } from "./modules/account/authenticateClient/AuthenticateClientController";
 import { AuthenticateDeliverymanController } from "./modules/account/authenticateDeliveryman/AuthenticateDeliverymanController";
 import { CreateClientController } from "./modules/clients/useCases/createClient/CreateClientController";
+import { FindAllDeliveriesController } from "./modules/clients/useCases/deliveries/FindAllDeliveriesController";
 import { CreateDeliveryController } from "./modules/deliveries/useCases/createDelivery/createDeliveryController";
 import { FindAllAvailableController } from "./modules/deliveries/useCases/findAllAvailable/FindAllAvailableController";
 import { UpdateDeliverymanController } from "./modules/deliveries/useCases/updateDeliveryman/UpdateDeliverymanController";
@@ -20,6 +21,8 @@ const createDeliverymanController = new CreateDeliverymanController();
 const createDeliveryController = new CreateDeliveryController();
 const findAllAvailableController = new FindAllAvailableController();
 const updateDeliverymanController = new UpdateDeliverymanController();
+
+const findAllDeliveriesClientController = new FindAllDeliveriesController();
 
 routes.post("/client/authenticate", authenticateClientController.handle);
 routes.post(
@@ -47,5 +50,7 @@ routes.put(
   ensureAuthenticateDeliveryman,
   updateDeliverymanController.handle
 );
+
+routes.get("/client/deliveries", findAllDeliveriesClientController.handle);
 
 export { routes };
